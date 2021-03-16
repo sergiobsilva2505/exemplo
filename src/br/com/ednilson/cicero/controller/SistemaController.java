0b08b60c71d53ef6168085e89de9ce24ed2f7c60
Sergio Bezerra <sergio.bezerra@outlook.com>
2021-03-16 19:35:32 -0300

    Testado e funcionando

--- src/br/com/ednilson/cicero/controller/SistemaController.java
+++ src/br/com/ednilson/cicero/controller/SistemaController.java
@@ -1,15 +1,15 @@
 package br.com.ednilson.cicero.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/sistema")
 public class SistemaController {
-	@RequestMapping(value = "/boasvindas", method = RequestMethod.GET)
+	@GetMapping(value = "/boasvindas")
 	public ModelAndView boasVindas(
 			@RequestParam(value = "nome", required = false, defaultValue = "Cícero Ednilson") String nome) {
 
